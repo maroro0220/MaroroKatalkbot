8a65409a520517843e972e9e4e8c1a7a9a693962
maroro0220 <maro1339@gamil.com>
2018-01-05 07:25:26 +0000

    arthousemomo today's schedule

--- Rails - Kakaotalk bot.md
+++ Rails - Kakaotalk bot.md
@@ -245,5 +245,84 @@ end
 
 ---
 
-[참고](https://github.com/och8808/kakao_bot_sample)
+# Module
+
+app-helpers에   parse.rb만들기
+
+```ruby
+module Parse
+  class Movie
+      def naver
+        url = "http://movie.naver.com/movie/running/current.nhn?view=list&tab=normal&order=reserve"
+        movie_html = RestClient.get(url)
+        doc = Nokogiri::HTML(movie_html)
+        movie_list=Array.new
+        # movie_rate=Array.new
+        movie_hash=Hash.new
+        doc.css('ul.lst_detail_t1 li').each do |movie| #ul의 lst클래스에 들어있는 li태그
+          movie_title = movie.css('dt a').text
+          movie_list << movie_title
+          #dt안에 있는 a tag의 text
+          # movie_rate << movie.css('dd.star dl.info_star dd div.star_t1 a span.num')
+          # movie_hash [ movie.css('dt a').text] = movie.css('dl.info_star span.num')
+          movie_hash[movie_title] = {
+            :star => movie.css('dl.info_star span.num').text,
+            :url => movie.css('div.thumb img').attribute('src').to_s
+          }
+          # movie.css('dd.star dl.info_star dd div.star_t1 a span.num').text
+          #<<는 movie_list배열에 하나씩 넣는다
+        end
+        sample_movie = movie_list.sample
+        bot_message = sample_movie +" - "+ movie_hash[sample_movie][:star]
+        img_url = movie_hash[sample_movie][:url]
+        return [bot_message, img_url]
+      end
+  end
+
+  class Animal
+    def cat
+      bot_message="나만 고양이 없어"
+      url = "http://thecatapi.com/api/images/get?format=xml&type=jpg"
+      cat_xml = RestClient.get(url)
+      doc = Nokogiri::XML(cat_xml)
+      img_url = doc.xpath("//url").text#url에 접근해서 안에 있는 text만 가져옴
+      return [bot_message, img_url] #없어도 됨. 자동으로 해줌
+    end
+  end
+end
+
+```
+
+controller에서 소스 가져와서 넣기
+
+```ruby
+#kakao_controller.rb  
+elsif user_message =='냥이' || user_message=='고양이'|| user_message=='ㄱㅇㅇ'
+      #app-helpers-parse.rb-Animal-cat
+      image = true
+      parser = Parse::Animal.new  #class로 접근하려고 ::
+      bot_message = parser.cat[0]
+      img_url = parser.cat[1]
+      # parser.cat=[bot_message, img_url]
+    elsif user_message =='영화'
+      image=true
+      parser=Parse::Movie.new
+      movie=parser.naver
+      bot_message = movie[0]
+      img_url = movie[1]
+```
+
+
+
+
+
+
+
+---
+
+
+
+[참고1](https://github.com/och8808/kakao_bot_sample)
+
+[참고2](https://github.com/classHANA?tab=repositories)
 
--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -1,3 +1,4 @@
+require 'parse'
 class KakaoController < ApplicationController
   def keyboard
     home_keyboard = {
@@ -20,52 +21,42 @@ class KakaoController < ApplicationController
     elsif user_message == '박민정'
         bot_message="♡♥♡♥♡♥♡♥"
     elsif user_message =='냥이' || user_message=='고양이'|| user_message=='ㄱㅇㅇ'
-      bot_message="나만 고양이 없어"
-      url = "http://thecatapi.com/api/images/get?format=xml&type=jpg"
-      cat_xml = RestClient.get(url)
-      doc = Nokogiri::XML(cat_xml)
-      img_url = doc.xpath("//url").text#url에 접근해서 안에 있는 text만 가져옴
+      #app-helpers-parse.rb-Animal-cat
       image = true
+      parser = Parse::Animal.new  #class로 접근하려고 ::
+      bot_message = parser.cat[0]
+      img_url = parser.cat[1]
+      # parser.cat=[bot_message, img_url]
     elsif user_message =='영화'
       image=true
-      url = "http://movie.naver.com/movie/running/current.nhn?view=list&tab=normal&order=reserve"
+      parser=Parse::Movie.new
+      movie=parser.naver
+      bot_message = movie[0]
+      img_url = movie[1]
+      # bot_message = sample_movie +" - "+ movie_hash[sample_movie]
+    elsif user_message =='모모'
+      url = "http://www.arthousemomo.co.kr"
       movie_html = RestClient.get(url)
       doc = Nokogiri::HTML(movie_html)
       movie_list=Array.new
-      # movie_rate=Array.new
+      time_list=Array.new
+      movie =""
       movie_hash=Hash.new
-      doc.css('ul.lst_detail_t1 li').each do |movie| #ul의 lst클래스에 들어있는 li태그
-        movie_title = movie.css('dt a').text
-        movie_list << movie_title
-        #dt안에 있는 a tag의 text
-        # movie_rate << movie.css('dd.star dl.info_star dd div.star_t1 a span.num')
-        # movie_hash [ movie.css('dt a').text] = movie.css('dl.info_star span.num')
-        movie_hash[movie_title] = {
-          :star => movie.css('dl.info_star span.num').text,
-          :url => movie.css('div.thumb img').attribute('src').to_s
-        }
-        # movie.css('dd.star dl.info_star dd div.star_t1 a span.num').text
-        #<<는 movie_list배열에 하나씩 넣는다
+
+      i=0
+      doc.css('div.time-box ul li').each do |m|
+        if i%2==0
+          time_list << m.text
+        else
+          movie_list << m.css('a').text
+        end
+        i=i+1
       end
-      sample_movie = movie_list.sample
-      bot_message = sample_movie +" - "+ movie_hash[sample_movie][:star]
-      img_url = movie_hash[sample_movie][:url]
-      # bot_message = sample_movie +" - "+ movie_hash[sample_movie]
-    # elsif user_message =='모모'
-    #   url = "http://www.arthousemomo.co.kr"
-    #   movie_html = RestClient.get(url)
-    #   doc = Nokogiri::HTML(movie_html)
-    #   # movie_list=Array.new
-    #   time_list=Array.new
-    #   movie_hash=Hash.new
-    #   doc.css('div.time-box ul').each do |movie|
-    #     time_list << movie.css('li.first')
-    #     # movie_list << movie.css('li a').text
-    #     movie_hash[time_list]
-    #   end
-    #   for i in time_list.length
-    #     bot_message = time_list[i] + movie_list[i]
-    #   end
+
+      for i in (0..time_list.length-1)
+        movie = movie + time_list[i].to_s  + " - " + movie_list[i].to_s+"\n"
+      end
+      bot_message = movie
     else
         bot_message="Nop. '메뉴' or '로또' or 'ㄱㅇㅇ'"
     end
