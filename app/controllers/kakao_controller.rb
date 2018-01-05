89d12c0c057388166731ee3475144722e450cbe2
maroro0220 <maro1339@gamil.com>
2018-01-05 02:50:44 +0000

    로또 기능 추가

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -1,7 +1,7 @@
 class KakaoController < ApplicationController
   def keyboard
     home_keyboard = {
-  :type => "hi" #해쉬형태로 바꿈. type: "text"이렇게도 됨. 원래 buttons였는데 text로 바꿈.
+  :type => "text" #해쉬형태로 바꿈. type: "text"이렇게도 됨. 원래 buttons였는데 text로 바꿈.
   #우리는 text입력을 받을거여서. button형태로 입력받으려면 buttons쓰면됨. button형태 입력은 질문 값이 버튼에 다 들어있음
   # :buttons => ["선택 1", "선택 2", "선택 3"]  # buttons: []
     }
